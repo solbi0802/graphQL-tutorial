1adc5d23317980bcbb1f3c2c76d488b047ab11a6
SolbiShin <sbshin.dev@gmail.com>
2022-02-04 22:52:58 +0900

    apollo client setup

--- apollo--2022/src/index.js
+++ apollo--2022/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/client";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
