9940d64911f31e23bce489a871b95e7f8b56622a
SolbiShin <sbshin.dev@gmail.com>
2022-02-04 23:03:33 +0900

    GET MOVIE Query

--- apollo--2022/src/apollo.js
+++ apollo--2022/src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://movieql2.vercel.app",
   cache: new InMemoryCache(),
 });
 
--- apollo--2022/src/routes/Home.js
+++ apollo--2022/src/routes/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const { loading, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "로딩중..";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
 };
 
 export default Home;
