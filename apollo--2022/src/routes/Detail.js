989baf6ff8c1f8e615d6b262bd5d283351aaaf4e
SolbiShin <sbshin.dev@gmail.com>
2022-02-08 00:13:02 +0900

    GET MOVIE Query

--- apollo--2022/src/routes/Detail.js
+++ apollo--2022/src/routes/Detail.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 const Detail = () => {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIES, {
+    variables: { id: +id },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
 
 export default Detail;
--- apollo--2022/src/routes/Home.js
+++ apollo--2022/src/routes/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
@@ -9,15 +11,55 @@ const GET_MOVIES = gql`
     }
   }
 `;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
 
 const Home = () => {
   const { loading, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return "로딩중..";
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2022</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
 };
 
 export default Home;
